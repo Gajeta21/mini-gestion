2976f740179609e6c06f2476c5fdf3d2110d240e
Gajeta21 <gabrielhdez2101@gmail.com>
2026-02-07 09:07:33 -0400

    Paso 3: endpoints para crear y listar tareas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Task(BaseModel):
+    title: str
+
+tasks = []
 
 @app.get("/")
 def read_root():
     return {"mensaje": "Hola mundo 🚀"}
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    return task
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
